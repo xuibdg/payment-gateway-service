284bb8d6ed967e63369e4894b5533fd624ace489
FadhilR <fadilrafq156@gmail.com>
2025-05-23 15:21:51 +0700

    replace UUID to String

--- src/main/java/com/core/payment_gateway_service/repository/PaymentGatewayCallbackRepository.java
+++ src/main/java/com/core/payment_gateway_service/repository/PaymentGatewayCallbackRepository.java
@@ -3,7 +3,5 @@ package com.core.payment_gateway_service.repository;
 import com.core.payment_gateway_service.entity.PaymentGatewayCallback;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
 public interface PaymentGatewayCallbackRepository extends JpaRepository<PaymentGatewayCallback, String> {
 }
