365f087ab29f1b4d706945aa5f3acdb46df328b4
mispahirlanhardiansyah <landthursmorn@gmail.com>
2025-06-03 08:46:28 +0700

    clean code remove not to used in any code

--- src/main/java/com/core/payment_gateway_service/dto/FlipResponse.java
+++ src/main/java/com/core/payment_gateway_service/dto/FlipResponse.java
@@ -1,4 +1,4 @@
-package com.core.payment_gateway_service.DTO;
+package com.core.payment_gateway_service.dto;
 
 import com.core.payment_gateway_service.entity.PaymentGatewayTransaction;
 import com.fasterxml.jackson.annotation.JsonFormat;
--- src/main/java/com/core/payment_gateway_service/repository/PaymentGatewayTransactionRepository.java
+++ src/main/java/com/core/payment_gateway_service/repository/PaymentGatewayTransactionRepository.java
@@ -3,7 +3,5 @@ package com.core.payment_gateway_service.repository;
 import com.core.payment_gateway_service.entity.PaymentGatewayTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
 public interface PaymentGatewayTransactionRepository extends JpaRepository<PaymentGatewayTransaction, String> {
 }
