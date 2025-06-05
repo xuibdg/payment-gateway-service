1dcb5b9cdaff7b32e0e461e7ff493f823dae4e0f
jau-spell <jauharram@gmail.com>
2025-06-05 13:54:05 +0700

    Add Scheduler Reconciliation status flip methode, add some annotations and repo

--- src/main/java/com/core/payment_gateway_service/entity/PaymentGateway.java
+++ src/main/java/com/core/payment_gateway_service/entity/PaymentGateway.java
@@ -12,8 +12,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+
 
 
 import java.sql.Timestamp;
--- src/main/java/com/core/payment_gateway_service/entity/PaymentGatewayConfig.java
+++ src/main/java/com/core/payment_gateway_service/entity/PaymentGatewayConfig.java
@@ -13,6 +13,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
